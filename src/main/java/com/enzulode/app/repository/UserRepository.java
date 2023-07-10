986c1931189aa350e2a036fe78332e5b2afdaf3b
Vladislav Andreev <andreevvlad443@gmail.com>
2023-07-10 03:29:41 +0300

    implemented findUserById method

--- src/main/java/com/enzulode/app/repository/UserRepository.java
+++ src/main/java/com/enzulode/app/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.enzulode.app.repository;
 
 import com.enzulode.app.model.User;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>
 {
+	Optional<User> findUserById(@NonNull Long id);
 }
