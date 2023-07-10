5b8f5cf93e2e80b1cddd7bb3806e983b1d60e4d5
Vladislav Andreev <andreevvlad443@gmail.com>
2023-07-10 03:28:22 +0300

    implemented an exception for user not found situation

---
+++ src/main/java/com/enzulode/app/exception/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package com.enzulode.app.exception;
+
+public class UserNotFoundException extends RuntimeException
+{
+	public UserNotFoundException(String message)
+	{
+		super(message);
+	}
+
+	public UserNotFoundException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+}
