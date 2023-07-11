616c5e3576a8452aec5b3cd5681b1a5c4e7db217
Vladislav Andreev <andreevvlad443@gmail.com>
2023-07-11 04:08:55 +0300

    UserService naming strategy refactorings

--- src/main/java/com/enzulode/app/service/UserServiceImpl.java
+++ src/main/java/com/enzulode/app/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.enzulode.app.exception.FailedToCreateUserException;
 import com.enzulode.app.exception.UserNotFoundException;
 import com.enzulode.app.model.User;
 import com.enzulode.app.repository.UserRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -12,11 +13,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class UserService
+public class UserServiceImpl implements UserService
 {
 	private final UserRepository userRepository;
 
-	public void createNewUser(User user)
+	@Override
+	public void save(@NonNull User user)
 	{
 		try
 		{
@@ -29,7 +31,8 @@ public class UserService
 		}
 	}
 
-	public User findUserById(Long id)
+	@Override
+	public User findById(@NonNull Long id)
 	{
 		log.info("Retrieving user by id");
 		var userOptional = userRepository.findUserById(id);
